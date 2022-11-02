98f2c23c553335f007f01d9bca8f33f9aff36935
neyssacadet <neyssacadet@gmail.com>
2022-11-02 06:25:09 -0500

    placed test file outside ofo package

--- src/com/sg/VendingMachine/test/VendingMachineDaoImplTest.java
+++ src/com/sg/VendingMachine/test/VendingMachineDaoImplTest.java
@@ -2,15 +2,9 @@ package com.sg.VendingMachine.test;
 
 import com.sg.VendingMachine.dao.VendingMachineDao;
 import com.sg.VendingMachine.dao.VendingMachineDaoImpl;
-import com.sg.VendingMachine.dto.Item;
 import org.junit.jupiter.api.*;
 
 import java.io.FileWriter;
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.TreeMap;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class VendingMachineDaoImplTest {
 
